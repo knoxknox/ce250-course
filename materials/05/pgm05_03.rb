3a380b6243f92d18f5b04b90868f9667637570e9
knoxknox <alexandr_y@mail.ru>
2019-02-06 20:00:58 +0200

    Merge module 05 parts 03-08

--- materials/05/pgm05_03.rb
+++ materials/05/pgm05_03.rb
@@ -1,3 +1,4 @@
+# 03
 class Container < AbstractObject
   include ::Enumerable
   attr_reader :count
@@ -17,6 +18,46 @@ class Container < AbstractObject
 
   def full?
     false
+  end
+
+end
+
+# 05
+class Container < AbstractObject
+  abstractmethod :iter
+
+  def each
+    i = iter
+    while i.more?
+      yield i.succ
+    end
+  end
+
+end
+
+# 06
+class Container < AbstractObject
+
+  def to_s
+    s = ""
+    each do |obj|
+      s << ", " if not s.empty?
+      s << obj.to_s
+    end
+    type.name + "{" + s + "}"
+  end
+
+end
+
+# 08
+class Container < AbstractObject
+
+  def accept(visitor)
+    assert { visitor.is_a?(Visitor) }
+    each do |obj|
+      break if visitor.done?
+      visitor.visit(obj)
+    end
   end
 
 end
