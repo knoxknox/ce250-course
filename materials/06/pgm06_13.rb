57036b850a81f487d2495ab69f6631cec7c308bb
knoxknox <alexandr_y@mail.ru>
2019-02-06 20:14:55 +0200

    Merge module 06 parts 13-14

--- materials/06/pgm06_13.rb
+++ materials/06/pgm06_13.rb
@@ -1,3 +1,4 @@
+# 13
 class QueueAsArray < Queue
 
   def initialize(size = 0)
@@ -16,6 +17,38 @@ class QueueAsArray < Queue
       end
       @count -= 1
     end
+  end
+
+end
+
+# 14
+class QueueAsArray < Queue
+
+  def head
+    raise ContainerEmpty if @count == 0
+    @array[@head]
+  end
+
+  def enqueue(obj)
+    raise ContainerFull if @count == @array.length
+    @tail = @tail + 1
+    if @tail == @array.length
+      @tail = 0
+    end
+    @array[@tail] = obj
+    @count += 1
+  end
+
+  def dequeue
+    raise ContainerEmpty if @count == 0
+    result = @array[@head]
+    @array[@head] = nil
+    @head = @head + 1
+    if @head == @array.length
+      @head = 0
+    end
+    @count -= 1
+    result
   end
 
 end
