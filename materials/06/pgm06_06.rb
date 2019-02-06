eb831cb24b980f5ac1c5fd0af4f1ce7f07c067b2
knoxknox <alexandr_y@mail.ru>
2019-02-06 20:09:42 +0200

    Merge module 06 parts 06-10

--- materials/06/pgm06_06.rb
+++ materials/06/pgm06_06.rb
@@ -1,3 +1,4 @@
+# 06
 class StackAsLinkedList < Stack
 
   def initialize
@@ -8,6 +9,70 @@ class StackAsLinkedList < Stack
   def purge
     @list.purge
     super
+  end
+
+end
+
+# 07
+class StackAsLinkedList < Stack
+
+  def push(obj)
+    @list.prepend(obj)
+    @count += 1
+  end
+
+  def pop
+    raise ContainerEmpty if @count == 0
+    result = @list.first
+    @list.extract(result)
+    @count -= 1
+    return result
+  end
+
+  def top
+    raise ContainerEmpty if @count == 0
+    return @list.first
+  end
+
+end
+
+# 08
+class StackAsLinkedList < Stack
+
+  def each(&block)
+    @list.each(&block)
+  end
+
+end
+
+# 10
+class StackAsLinkedList < Stack
+
+  attr_reader :list
+
+  class Iterator < Opus8::Iterator
+
+    def initialize(stack)
+      @position = stack.list.head
+    end
+
+    def more?
+      not @position.nil?
+    end
+
+    def succ
+      if more?
+        result = @position.datum
+        @position = @position.succ
+      else
+        result = nil
+      end
+      return result
+    end
+  end
+
+  def iter
+    Iterator.new(self)
   end
 
 end
--- materials/06/pgm06_08.rb
+++
@@ -1,7 +0,0 @@
-class StackAsLinkedList < Stack
-
-  def each(&block)
-    @list.each(&block)
-  end
-
-end
