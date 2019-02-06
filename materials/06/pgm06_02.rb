74550ff4ef392c1e73f9914cb0c5eb065dc91257
knoxknox <alexandr_y@mail.ru>
2019-02-06 20:07:25 +0200

    Merge module 06 parts 02-05

--- materials/06/pgm06_02.rb
+++ materials/06/pgm06_02.rb
@@ -1,3 +1,4 @@
+# 02
 class StackAsArray < Stack
 
   def initialize(size = 0)
@@ -10,6 +11,75 @@ class StackAsArray < Stack
       @count -= 1
       @array[@count] = nil
     end
+  end
+
+end
+
+# 03
+class StackAsArray < Stack
+
+  def push(obj)
+    raise ContainerFull if @count == @array.length
+    @array[@count] = obj
+    @count += 1
+  end
+
+  def pop
+    raise ContainerEmpty if @count == 0
+    @count -= 1
+    result = @array[@count]
+    @array[@count] = nil
+    return result
+  end
+
+  def top
+    raise ContainerEmpty if @count == 0
+    return @array[@count - 1]
+  end
+
+end
+
+# 04
+class StackAsArray < Stack
+
+  def each
+    for i in 0 ... @count
+      yield @array[i]
+    end
+  end
+
+end
+
+# 05
+class StackAsArray < Stack
+
+  attr_reader :array
+
+  class Iterator < Opus8::Iterator
+
+    def initialize(stack)
+      @stack = stack
+      @position = 0
+    end
+
+    def more?
+      @position < @stack.count
+    end
+
+    def succ
+      if more?
+        assert { more? }
+        result = @stack.array[@position]
+        @position += 1
+      else
+        result = nil
+      end
+      return result
+    end
+  end
+
+  def iter
+    Iterator.new(self)
   end
 
 end
--- materials/06/pgm06_04.rb
+++
@@ -1,9 +0,0 @@
-class StackAsArray < Stack
-
-  def each
-    for i in 0 ... @count
-      yield @array[i]
-    end
-  end
-
-end
