6806ba72bc56e14a0f6f8cf5f339bced80c82807
knoxknox <alexandr_y@mail.ru>
2019-02-06 20:16:53 +0200

    Merge module 06 parts 19-20

--- materials/06/pgm06_19.rb
+++ materials/06/pgm06_19.rb
@@ -1,3 +1,4 @@
+# 19
 class DequeAsArray < QueueAsArray
 
   alias_method :queueHead, :head
@@ -22,5 +23,30 @@ class DequeAsArray < QueueAsArray
   end
 
   alias_method :dequeueHead, :dequeue
+
+end
+
+# 20
+class DequeAsArray < QueueAsArray
+
+  def tail
+    raise ContainerEmpty if @count == 0
+    @array[@tail]
+  end
+
+  alias_method :enqueueTail, :enqueue
+
+  def dequeueTail
+    raise ContainerEmpty if @count == 0
+    result = @array[@tail]
+    @array[@tail] = nil
+    if @tail == 0
+      @tail = @array.length - 1
+    else
+      @tail = @tail - 1
+    end
+    @count -= 1
+    result
+  end
 
 end
