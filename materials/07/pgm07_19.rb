55f13b0524aedd137f9b8d8fd44ededbf50a22fd
knoxknox <alexandr_y@mail.ru>
2019-02-07 19:45:57 +0200

    Merge module 07 parts 19-31

--- materials/07/pgm07_19.rb
+++ materials/07/pgm07_19.rb
@@ -1,3 +1,4 @@
+# 19
 class Polynomial < Container
 
   class Term < AbstractObject
@@ -27,6 +28,52 @@ class Polynomial < Container
       else
         @coefficient = 0
       end
+    end
+
+  end
+
+end
+
+# 20
+class Polynomial < Container
+
+  def initialize
+    super
+  end
+
+  abstractmethod :addTerm
+
+  abstractmethod :differentiate!
+
+  abstractmethod :+
+
+end
+
+# 22
+class Polynomial < Container
+
+  def differentiate!
+    each { |term| term.differentiate! }
+    zeroTerm = find(Term.new(0, 0))
+    if not zeroTerm.nil?
+      withdraw(zeroTerm)
+    end
+  end
+
+end
+
+# 31
+class Polynomial < Container
+
+  class Term < AbstractObject
+
+    def clone
+      Term.new(@coefficient, @exponent)
+    end
+
+    def +(term)
+      assert { @exponent == term.exponent }
+      return Term.new(@coefficient + term.coefficient, @exponent)
     end
 
   end
