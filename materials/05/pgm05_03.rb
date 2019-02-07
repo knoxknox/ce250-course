016055cb6f9b9f996a19878b1c9f761f5786b61a
knoxknox <alexandr_y@mail.ru>
2019-02-07 19:57:16 +0200

    Merge module 08 part 04

--- materials/05/pgm05_03.rb
+++ materials/05/pgm05_03.rb
@@ -61,3 +61,17 @@ class Container < AbstractObject
   end
 
 end
+
+# 08:04
+class Container < AbstractObject
+
+  def hash
+    result = 0
+    each do |obj|
+      result += obj.hash
+    end
+    result += type.name.hash
+    return result
+  end
+
+end
