18d84f46c8166522fcad42e394c6abcdeb768d03
knoxknox <alexandr_y@mail.ru>
2019-02-07 19:49:01 +0200

    Merge module 07 parts 29-30

--- materials/07/pgm07_29.rb
+++ materials/07/pgm07_29.rb
@@ -1,9 +1,31 @@
+# 29
 class SortedListAsLinkedList < OrderedListAsLinkedList
 
   include SortedListMethods
 
   def initialize
     super
+  end
+
+end
+
+# 30
+class SortedListAsLinkedList < OrderedListAsLinkedList
+
+  def insert(obj)
+    prevPtr = nil
+    ptr = @linkedList.head
+    while not ptr.nil?
+      break if ptr.datum >= obj
+      prevPtr = ptr
+      ptr = ptr.succ
+    end
+    if prevPtr.nil?
+      @linkedList.prepend(obj)
+    else
+      prevPtr.insertAfter(obj)
+    end
+    @count += 1
   end
 
 end
