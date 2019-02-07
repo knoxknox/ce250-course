1084c7ed5215936787d9e7e23d13661c4ae9e216
knoxknox <alexandr_y@mail.ru>
2019-02-07 19:54:28 +0200

    Merge module 08 parts 01-03

--- materials/08/pgm08_01.rb
+++ materials/08/pgm08_01.rb
@@ -1,3 +1,4 @@
+# 01
 class Fixnum
 
   BITS = 31
@@ -10,6 +11,35 @@ class Integer
 
   def hash
     return self & Fixnum::MAX
+  end
+
+end
+
+# 02
+class Float
+
+  def hash
+    (m, e) = Math.frexp(self)
+    mprime = ((m.abs - 0.5) * (1 << 52)).to_i
+    return mprime >> 21
+  end
+
+end
+
+# 03
+class String
+
+  SHIFT = 6
+
+  MASK = ~0 << (Fixnum::BITS - SHIFT)
+
+  def hash
+    result = 0
+    each_byte do |c|
+      result = ((result & MASK) ^
+      (result << SHIFT) ^ c) & Fixnum::MAX
+    end
+    return result
   end
 
 end
--- materials/08/pgm08_02.rb
+++
@@ -1,9 +0,0 @@
-class Float
-
-  def hash
-    (m, e) = Math.frexp(self)
-    mprime = ((m.abs - 0.5) * (1 << 52)).to_i
-    return mprime >> 21
-  end
-
-end
