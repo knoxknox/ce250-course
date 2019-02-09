6844bd0969f70c73f4b51a76235975daae3a3fb1
knoxknox <alexandr_y@mail.ru>
2019-02-09 16:48:57 +0200

    Merge module 09 parts 03-06

--- materials/09/pgm09_03.rb
+++ materials/09/pgm09_03.rb
@@ -1,11 +1,73 @@
+# 03
 class PrePostVisitor < Visitor
-
   abstractmethod :preVisit
-
   abstractmethod :inVisit
-
   abstractmethod :postVisit
-
   alias_method :visit, :inVisit
+end
 
+# 04
+class PreOrder < PrePostVisitor
+  def initialize(visitor)
+    super()
+    @visitor = visitor
+  end
+
+  def preVisit(obj)
+    @visitor.visit(obj)
+  end
+
+  def inVisit(obj)
+  end
+
+  def postVisit(obj)
+  end
+
+  def done?
+    @visitor.done?
+  end
+end
+
+# 05
+class InOrder < PrePostVisitor
+  def initialize(visitor)
+    super()
+    @visitor = visitor
+  end
+
+  def preVisit(obj)
+  end
+
+  def inVisit(obj)
+    @visitor.visit(obj)
+  end
+
+  def postVisit(obj)
+  end
+
+  def done?
+    @visitor.done?
+  end
+end
+
+# 06
+class PostOrder < PrePostVisitor
+  def initialize(visitor)
+    super()
+    @visitor = visitor
+  end
+
+  def preVisit(obj)
+  end
+
+  def inVisit(obj)
+  end
+
+  def postVisit(obj)
+    @visitor.visit(obj)
+  end
+
+  def done?
+    @visitor.done?
+  end
 end
