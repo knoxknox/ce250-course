dd99935bbc8c1197faca19494612f56a2438a4ec
knoxknox <alexandr_y@mail.ru>
2019-02-09 16:50:24 +0200

    Merge module 09 parts 12-14

--- materials/09/pgm09_12.rb
+++ materials/09/pgm09_12.rb
@@ -1,3 +1,4 @@
+# 12
 class NaryTree < Tree
 
   def initialize(*args)
@@ -22,6 +23,59 @@ class NaryTree < Tree
   def purge
     @key = nil
     @subtree = nil
+  end
+
+end
+
+# 13
+class NaryTree < Tree
+
+  def empty?
+    @key.nil?
+  end
+
+  def key
+    raise StateError if empty?
+    return @key
+  end
+
+  def attachKey(obj)
+    raise StateError if not empty?
+    @key = obj
+    @subtree = Array.new(degree)
+    for i in 0 ... degree
+      @subtree[i] = NaryTree.new(degree)
+    end
+  end
+
+  def detachKey
+    raise StateError if not leaf?
+    result = @key
+    @key = nil
+    @subtree = nil
+    return result
+  end
+
+end
+
+# 14
+class NaryTree < Tree
+
+  def getSubtree(i)
+    raise StateError if empty?
+    return @subtree[i]
+  end
+
+  def attachSubtree(i, t)
+    raise StateError if empty? or not @subtree[i].empty?
+    @subtree[i] = t
+  end
+
+  def detachSubtree(i)
+    raise StateError if empty?
+    result = @subtree[i]
+    @subtree[i] = NaryTree.new(degree)
+    return result
   end
 
 end
