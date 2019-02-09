eadfbad2f9c52bd7986137a0a612829c161568ff
knoxknox <alexandr_y@mail.ru>
2019-02-09 16:52:20 +0200

    Merge module 09 parts 15-18

--- materials/09/pgm09_15.rb
+++ materials/09/pgm09_15.rb
@@ -1,3 +1,4 @@
+# 15
 class BinaryTree < Tree
 
   def initialize(*args)
@@ -24,6 +25,59 @@ class BinaryTree < Tree
     @key = nil
     @left = nil
     @right = nil
+  end
+
+end
+
+# 16
+class BinaryTree < Tree
+
+  def left
+    raise StateError if empty?
+    @left
+  end
+
+  def right
+    raise StateError if empty?
+    @right
+  end
+
+end
+
+# 17
+class BinaryTree < Tree
+
+  def depthFirstTraversal(&block)
+    if not empty?
+      yield (key, PREVISIT)
+      left.depthFirstTraversal(&block)
+      yield (key, INVISIT)
+      right.depthFirstTraversal(&block)
+      yield (key, POSTVISIT)
+    end
+  end
+
+end
+
+# 18
+class BinaryTree < Tree
+
+  def compareTo(bt)
+    assert { is_a?(obj.type) }
+    if empty?
+      if bt.empty?
+        return 0
+      else
+        return -1
+      end
+    elsif bt.empty?
+      return 1
+    else
+      result = @key <=> bt._key
+      result = @left <=> bt._left if result == 0
+      result = @right <=> bt._right if result == 0
+      return result
+    end
   end
 
 end
