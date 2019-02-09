e84ab888e1697771ef38586881350464edccf670
knoxknox <alexandr_y@mail.ru>
2019-02-09 17:08:54 +0200

    Merge module 10 parts 13-18

--- materials/10/pgm10_13.rb
+++ materials/10/pgm10_13.rb
@@ -1,3 +1,4 @@
+# 13
 class MWayTree < SearchTree
 
   def initialize(m)
@@ -9,6 +10,140 @@ class MWayTree < SearchTree
 
   def m
     @subtree.length
+  end
+
+end
+
+# 14
+class MWayTree < SearchTree
+
+    def depthFirstTraversal(&block)
+        if not empty?
+      for i in 1 .. @count
+    yield (@key[i], PREVISIT)
+      end
+      @subtree[0].depthFirstTraversal(&block)
+            for i in 1 .. @count
+    yield (@key[i], INVISIT)
+    @subtree[i].depthFirstTraversal(&block)
+      end
+      for i in 1 .. @count
+    yield (@key[i], POSTVISIT)
+      end
+  end
+    end
+
+end
+
+# 15
+class MWayTree < SearchTree
+
+  def find(obj)
+    return nil if empty?
+    i = @count
+    while i > 0
+      diff = obj <=> @key[i]
+      return @key[i] if diff == 0
+      break if diff > 0
+      i = i - 1
+    end
+    return @subtree[i].find(obj)
+  end
+
+end
+
+# 16
+class MWayTree < SearchTree
+
+  def findIndex(obj)
+    return 0 if empty? or obj < @key[1]
+    left = 1
+    right = @count
+    while left < right
+      middle = (left + right + 1) / 2
+      if obj < @key[middle]
+        right = middle - 1
+      else
+        left = middle
+      end
+    end
+    return left
+  end
+
+  def find(obj)
+    return nil if empty?
+    index = findIndex(obj)
+    if index != 0 and @key[index] == obj
+      return @key[index]
+    else
+      return @subtree[index].find(obj)
+    end
+  end
+
+end
+
+# 17
+class MWayTree < SearchTree
+
+  def insert(obj)
+    if empty?
+      @subtree[0] = MWayTree.new(m)
+      @key[1] = obj
+      @subtree[1] = MWayTree.new(m)
+      @count = 1
+    else
+      index = findIndex(obj)
+      raise ValueError if index != 0 and @key[index] == obj
+      if not full?
+        i = @count
+        while i > index
+          @key[i + 1] = @key[i]
+          @subtree[i + 1] = @subtree[i]
+          i = i - 1
+        end
+        @key[index + 1] = obj
+        @subtree[index + 1] = MWayTree.new(m)
+        @count = @count + 1
+      else
+        @subtree[index].insert(obj)
+      end
+    end
+  end
+
+end
+
+# 18
+class MWayTree < SearchTree
+
+  def withdraw(obj)
+    raise ArgumentError if empty?
+    index = findIndex(obj)
+    if index != 0 and @key[index] == obj
+      if not @subtree[index - 1].empty?
+        max = @subtree[index - 1].max
+        @key[index] = max
+        @subtree[index - 1].withdraw(max)
+      elsif not @subtree[index].empty?
+        min = @subtree[index].min
+        @key[index] = min
+        @subtree[index].withdraw(min)
+      else
+        @count = @count - 1
+        i = index
+        while i <= @count
+          @key[i] = @key[i + 1]
+          @subtree[i] = @subtree[i + 1]
+          i = i + 1
+        end
+        @key[i] = nil
+        @subtree[i] = nil
+        if @count == 0
+          @subtree[0] = nil
+        end
+      end
+    else
+      @subtree[index].withdraw(obj)
+    end
   end
 
 end
