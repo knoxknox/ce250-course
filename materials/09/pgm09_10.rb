5c12e24ec95b817d34b585b1e079529d57f8902e
knoxknox <alexandr_y@mail.ru>
2019-02-09 16:49:41 +0200

    Merge module 09 parts 10-11

--- materials/09/pgm09_10.rb
+++ materials/09/pgm09_10.rb
@@ -1,3 +1,4 @@
+# 10
 class GeneralTree < Tree
 
   def initialize(key)
@@ -10,6 +11,33 @@ class GeneralTree < Tree
   def purge
     @list.purge
     @degree = 0
+  end
+
+end
+
+# 11
+class GeneralTree < Tree
+
+  attr_reader :key
+
+  def getSubtree(i)
+    raise IndexError if not (0 .. degree) === i
+    ptr = @list.head
+    for j in 0 ... i
+      ptr = ptr.succ
+    end
+    ptr.datum
+  end
+
+  def attachSubtree(t)
+    @list.append(t)
+    @degree += 1
+  end
+
+  def detachSubtree(t)
+    @list.extract(t)
+    @degree -= 1
+    return t
   end
 
 end
