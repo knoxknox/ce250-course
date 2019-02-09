5efd8c3145c6afc943246d8ae2a44dcc9ed55113
knoxknox <alexandr_y@mail.ru>
2019-02-09 17:10:10 +0200

    Merge module 10 parts 19-21

--- materials/10/pgm10_19.rb
+++ materials/10/pgm10_19.rb
@@ -1,3 +1,4 @@
+# 19
 class BTree < MWayTree
 
   def initialize(m)
@@ -11,5 +12,59 @@ class BTree < MWayTree
   end
 
   attr_accessor :parent
+
+end
+
+# 20
+class BTree < MWayTree
+
+  def insert(obj)
+    if empty?
+      if @parent.nil?
+        attachSubtree(0, BTree.new(m))
+        @key[1] = obj
+        attachSubtree(1, BTree.new(m))
+        @count = 1
+      else
+        @parent.insertUp(obj, BTree.new(m))
+      end
+    else
+      index = findIndex(obj)
+      raise ArgumentError if index != 0 and @key == obj
+      @subtree[index].insert(obj)
+    end
+  end
+
+end
+
+# 21
+class BTree < MWayTree
+
+  def insertUp(obj, child)
+    index = findIndex(obj)
+    if not full?
+      insertPair(index + 1, obj, child)
+      @count = @count + 1
+    else
+      extraKey, extraTree = insertPair(index+1, obj, child)
+      if @parent.nil?
+        left = BTree.new(m)
+        right = BTree.new(m)
+        left.attachLeftHalfOf(self)
+        right.attachRightHalfOf(self)
+        right.insertUp(extraKey, extraTree)
+        attachSubtree(0, left)
+        @key[1] = @key[(m + 1)/2]
+        attachSubtree(1, right)
+        @count = 1
+      else
+        @count = (m + 1)/2 - 1
+        right = BTree.new(m)
+        right.attachRightHalfOf(self)
+        right.insertUp(extraKey, extraTree)
+        @parent.insertUp(@key[(m + 1)/2], right)
+      end
+    end
+  end
 
 end
