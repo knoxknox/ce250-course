6be2d7f49ea433b7af0deedd9b777dc93ab4d533
knoxknox <alexandr_y@mail.ru>
2019-02-10 20:12:38 +0200

    Merge module 12 parts 02-05

--- materials/12/pgm12_02.rb
+++ materials/12/pgm12_02.rb
@@ -1,3 +1,4 @@
+# 02
 class SetAsArray < Set
 
   def initialize(n)
@@ -10,5 +11,80 @@ class SetAsArray < Set
 
   attr_accessor :array
   protected :array
+
+end
+
+# 03
+class SetAsArray < Set
+
+  def insert(item)
+    @array[item] = true
+  end
+
+  def member?(item)
+    @array[item]
+  end
+
+  def withdraw(item)
+    @array[item] = false
+  end
+
+end
+
+# 04
+class SetAsArray < Set
+
+  def |(set)
+    assert { set.is_a?(SetAsArray) }
+    assert { @universeSize == set.universeSize }
+    result = SetAsArray.new(@universeSize)
+    for i in 0 ... @universeSize
+      result.array[i] = (@array[i] or set.array[i])
+    end
+    return result
+  end
+
+  def &(set)
+    assert { set.is_a?(SetAsArray) }
+    assert { @universeSize == set.universeSize }
+    result = SetAsArray.new(@universeSize)
+    for i in 0 ... @universeSize
+      result.array[i] = (@array[i] and set.array[i])
+    end
+    return result
+  end
+
+  def -(set)
+    assert { set.is_a?(SetAsArray) }
+    assert { @universeSize == set.universeSize }
+    result = SetAsArray.new(@universeSize)
+    for i in 0 ... @universeSize
+      result.array[i] = (@array[i] and not set.array[i])
+    end
+    return result
+  end
+
+end
+
+# 05
+class SetAsArray < Set
+
+  def ==(set)
+    assert { set.is_a?(SetAsArray) }
+    assert { @universeSize == set.universeSize }
+    for i in 0 ... @universeSize
+      return false if @array[i] != set.array[i]
+    end
+    return true
+  end
+
+  def <=(set)
+    assert { set.is_a?(SetAsArray) }
+    assert { @universeSize == set.universeSize }
+    for i in 0 ... @universeSize
+      return false if self_array[i] and not set.array[i]
+    end
+    return true
+  end
 
 end
