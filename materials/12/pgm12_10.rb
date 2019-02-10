9e5bcf250aee025be159b095b33a3be196a40366
knoxknox <alexandr_y@mail.ru>
2019-02-10 20:15:06 +0200

    Merge module 12 parts 10-12

--- materials/12/pgm12_10.rb
+++ materials/12/pgm12_10.rb
@@ -1,3 +1,4 @@
+# 10
 class MultisetAsArray < Multiset
 
   def initialize(n)
@@ -11,5 +12,60 @@ class MultisetAsArray < Multiset
   attr_accessor :array
   public :array
   protected :array=
+
+end
+
+# 11
+class MultisetAsArray < Multiset
+
+  def insert(item)
+    @array[item] += 1
+  end
+
+  def withdraw(item)
+    raise ArgumentEerror if @array[item] == 0
+    @array[item] -= 1
+  end
+
+  def member?(item)
+    @array[item] > 0
+  end
+
+end
+
+# 12
+class MultisetAsArray < Multiset
+
+  def |(set)
+    assert { set.is_a?(MultisetAsArray) }
+    assert { @universeSize == set.universeSize }
+    result = MultisetAsArray.new(@universeSize)
+    for i in 0 ... @universeSize
+      result.array[i] = @array[i] + set.array[i]
+    end
+    return result
+  end
+
+  def &(set)
+    assert { set.is_a?(MultisetAsArray) }
+    assert { @universeSize == set.universeSize }
+    result = MultisetAsArray.new(@universeSize)
+    for i in 0 ... @universeSize
+      result.array[i] = [@array[i], set.array[i]].min
+    end
+    return result
+  end
+
+  def -(set)
+    assert { set.is_a?(MultisetAsArray) }
+    assert { @universeSize == set.universeSize }
+    result = MultisetAsArray.new(@universeSize)
+    for i in 0 ... @universeSize
+      if set.array[i] <= @array[i]
+        result.array[i] = @array[i] - set.array[i]
+      end
+    end
+    return result
+  end
 
 end
