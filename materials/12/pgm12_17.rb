e986b49e01aa5fa95bc28e442c02798c3d684529
knoxknox <alexandr_y@mail.ru>
2019-02-10 20:17:27 +0200

    Merge module 12 parts 17-20

--- materials/12/pgm12_17.rb
+++ materials/12/pgm12_17.rb
@@ -1,3 +1,4 @@
+# 17
 class PartitionAsForest < Partition
 
   class PartitionTree < Tree
@@ -17,6 +18,45 @@ class PartitionAsForest < Partition
     attr_accessor :rank
     attr_accessor :count
 
+  end
+
+end
+
+# 18
+class PartitionAsForest < Partition
+
+  def initialize(n)
+    super
+    @array = Array.new(@universeSize)
+    for item in 0 ... @universeSize
+      @array[item] = PartitionTree.new(self, item)
+    end
+    @count = @universeSize
+  end
+
+end
+
+# 19
+class PartitionAsForest < Partition
+
+  def find(item)
+    ptr = @array[item]
+    while not ptr.parent.nil?
+      ptr = ptr.parent
+    end
+    return ptr
+  end
+
+end
+
+# 20
+class PartitionAsForest < Partition
+
+  def join(s, t)
+    assert { member?(s) and s.parent.nil? and
+    member?(t) and t.parent.nil? and not s.equal?(t) }
+    t.parent = s
+    @count -= 1
   end
 
 end
