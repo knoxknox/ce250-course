bef9edb6a2e1889c3fa8d4b6b768b54201665306
knoxknox <alexandr_y@mail.ru>
2019-02-10 20:15:50 +0200

    Merge module 12 parts 13-15

--- materials/12/pgm12_13.rb
+++ materials/12/pgm12_13.rb
@@ -1,3 +1,4 @@
+# 13
 class MultisetAsLinkedList < Multiset
 
   def initialize(n)
@@ -8,5 +9,56 @@ class MultisetAsLinkedList < Multiset
   attr_accessor :list
   public :list
   protected :list=
+
+end
+
+# 14
+class MultisetAsLinkedList < Multiset
+
+  def |(set)
+    assert { set.is_a?(MultisetAsLinkedList) }
+    assert { @universeSize == set.universeSize }
+    result = MultisetAsLinkedList.new(@universeSize)
+    p = @list.head
+    q = set.list.head
+    while not p.nil? and not q.nil?
+      if p.datum <= q.datum
+        result.list.append(p.datum)
+        p = p.succ
+      else
+        result.list.append(q.datum)
+        q = q.succ
+      end
+    end
+    while not p.nil?
+      result.list.append(p.datum)
+      p = p.succ
+    end
+    while not q.nil?
+      result.list.append(q.datum)
+      q = q.succ
+    end
+    return result
+  end
+
+end
+
+# 15
+class MultisetAsLinkedList < Multiset
+
+  def &(set)
+    assert { set.is_a?(MultisetAsLinkedList) }
+    assert { @universeSize == set.universeSize }
+    result = MultisetAsLinkedList.new(@universeSize)
+    p = @list.head
+    q = set.list.head
+    while not p.nil? and not q.nil?
+      diff = p.datum - q.datum
+      result.list.append(p.datum) if diff == 0
+      p = p.succ if diff <= 0
+      q = q.succ if diff >= 0
+    end
+    return result
+  end
 
 end
