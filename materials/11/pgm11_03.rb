4b73beafe796dae86e235ecb7bd325b4f9d0f36b
knoxknox <alexandr_y@mail.ru>
2019-02-10 20:06:04 +0200

    Merge module 11 parts 03-06

--- materials/11/pgm11_03.rb
+++ materials/11/pgm11_03.rb
@@ -1,3 +1,4 @@
+# 03
 class BinaryHeap < PriorityQueue
 
   def initialize(length = 0)
@@ -10,6 +11,57 @@ class BinaryHeap < PriorityQueue
       @array[@count] = nil
       @count -= 1
     end
+  end
+
+end
+
+# 04
+class BinaryHeap < PriorityQueue
+
+  def enqueue(obj)
+    raise ContainerFull if @count == @array.length
+    @count += 1
+    i = @count
+    while i > 1 and @array[i/2] > obj
+      @array[i] = @array[i / 2]
+      i /= 2
+    end
+    @array[i] = obj
+  end
+
+end
+
+# 05
+class BinaryHeap < PriorityQueue
+
+  def min
+    raise ContainerEmpty if @count == 0
+    @array[1]
+  end
+
+end
+
+# 06
+class BinaryHeap < PriorityQueue
+
+  def dequeueMin
+    raise ContainerEmpty if @count == 0
+    result = @array[1]
+    last = @array[@count]
+    @count -= 1
+    i = 1
+    while 2 * i < @count + 1
+      child = 2 * i
+      if child + 1 < @count + 1 \
+                and @array[child + 1] < @array[child]
+        child += 1
+      end
+      break if last <= @array[child]
+      @array[i] = @array[child]
+      i = child
+    end
+    @array[i] = last
+    return result
   end
 
 end
--- materials/11/pgm11_05.rb
+++
@@ -1,8 +0,0 @@
-class BinaryHeap < PriorityQueue
-
-  def min
-    raise ContainerEmpty if @count == 0
-    @array[1]
-  end
-
-end
