22e3549cdbe891b5fe8acec4d69ae8c5e1938a61
knoxknox <alexandr_y@mail.ru>
2019-02-17 16:18:09 +0200

    Merge module 16 parts 06-14

--- materials/16/pgm16_06.rb
+++ materials/16/pgm16_06.rb
@@ -1,3 +1,4 @@
+# 06
 module DigraphMethods
 
   abstractmethod :stronglyConnected?
@@ -13,5 +14,57 @@ class Digraph < Graph
   end
 
   include DigraphMethods
+
+end
+
+# 11
+module DigraphMethods
+
+  def topologicalOrderTraversal
+    inDegree = Array.new(numberOfVertices)
+    for v in 0 ... numberOfVertices
+      inDegree[v] = 0
+    end
+    edges { |e| inDegree[e.v1.number] += 1 }
+    queue = QueueAsLinkedList.new
+    for v in 0 ... numberOfVertices
+      queue.enqueue(self[v]) if inDegree[v] == 0
+    end
+    while not queue.empty?
+      v = queue.dequeue
+      yield v
+      v.successors do |to|
+        inDegree[to.number] -= 1
+        queue.enqueue(to) if inDegree[to.number] == 0
+      end
+    end
+  end
+
+end
+
+# 13
+module DigraphMethods
+
+  def stronglyConnected?
+    for v in 0 ... numberOfVertices
+      count = 0
+      depthFirstTraversal(0) do |obj, mode|
+        count += 1 if mode == Graph::PREVISIT
+      end
+      return false if count != numberOfVertices
+    end
+    return true
+  end
+
+end
+
+# 14
+module DigraphMethods
+
+  def cyclic?
+    count = 0
+    topologicalOrderTraversal { |obj| count += 1 }
+    count != numberOfVertices
+  end
 
 end
--- materials/16/pgm16_14.rb
+++
@@ -1,9 +0,0 @@
-module DigraphMethods
-
-  def cyclic?
-    count = 0
-    topologicalOrderTraversal { |obj| count += 1 }
-    count != numberOfVertices
-  end
-
-end
